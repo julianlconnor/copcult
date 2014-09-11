4995010c38cae413826cb2915886e4e53e7c7350
Julian Connor <julian.connor@venmo.com>
2014-09-11 13:04:06 -0400

    Remove marketing site.

--- apps/marketing/helpers/bower.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-
-module.exports.bower_path = function(addon) {
-  return path.join('/w/shared/lib', addon);
-};
--- apps/marketing/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(path.join(process.cwd(), 'build/marketing/')));
-app.use('/marketing/', express.static(path.join(__dirname)));
-
-module.exports = app;
-
