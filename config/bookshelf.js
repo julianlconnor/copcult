5412be7782b1d91a7f6d1412f0000db5f9819911
Julian Connor <julian.connor@venmo.com>
2014-09-24 13:36:27 -0400

    Change db connections.

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -2,17 +2,25 @@
 * DB Init.
 */
 var bookshelf = null;
+var knexConfig = require('../knexfile');
 
 module.exports = function() {
 
   if ( !bookshelf ) {
-    var knex = require('knex')({
-      client: 'sqlite3',
-      connection: {
-        filename: './mydb.sqlite'
-      }
-    });
-    bookshelf = require('bookshelf')(knex);
+    var connection;
+    var env = process.env.NODE_ENV.toLowerCase();
+
+    if ( env === 'development' ) {
+      connection = knexConfig.development.connection;
+    } else {
+      connection = knexConfig.staging.connection;
+    }
+
+    bookshelf = require('bookshelf')(require('knex')({
+      client: 'pg',
+      debug: true,
+      connection: connection
+    }));
   }
 
   return bookshelf;
--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,33 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './mydb.sqlite'
+      host: '127.0.0.1',
+      port: '5432',
+      user: 'julianconnor',
+      password: '',
+      database: 'postgres'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE
     }
   }
 
