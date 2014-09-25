7e95c2865a885d75a0d90975273f92e9540f21bb
Julian Connor <julian.connor@venmo.com>
2014-09-25 12:33:03 -0400

    Clean up stuff we don't need.

--- grunt/options/clean.js
+++ grunt/options/clean.js
@@ -1,7 +1,4 @@
 module.exports = {
   dist: ['tmp/'],
   gitHooks: ['.git/hooks/pre-commit'],
-
-  marketingSite: ['build/marketing'],
-  marketingArtifacts: ['build/marketing/apps']
 };
--- grunt/options/copy.js
+++ grunt/options/copy.js
@@ -1,28 +1,2 @@
 module.exports = {
-  images: {
-    files: [{
-      expand: true,
-      cwd: 'apps/shared',
-      src: ['images/**'],
-      dest: 'build/'
-    }]
-  },
-
-  yearInReview: {
-    files: [{
-      expand: true,
-      cwd: 'apps/year_in_review/public/js',
-      src: ['year_in_review.min.js'],
-      dest: 'build/js/'
-    }]
-  },
-
-  marketingSite: {
-    files: [{
-      expand: true,
-      cwd: 'build/marketing/apps/marketing/pages/',
-      src: ['**'],
-      dest: 'build/marketing/'
-    }]
-  }
 };
