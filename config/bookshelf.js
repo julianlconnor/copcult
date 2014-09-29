14398c1978714b324db7efb05c97a1af0e9f73d0
Julian Connor <julian.connor@venmo.com>
2014-09-29 15:45:47 -0400

    Dynamic debug flag for knex.

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -10,12 +10,14 @@ module.exports = function() {
   if ( !bookshelf ) {
     var env;
     var connection;
+    var debug = false;
 
     if ( process.env.TESTING ) {
       connection = knexConfig.test.connection;
     } else {
       env = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
       if ( env === 'development' ) {
+        debug = true;
         connection = knexConfig.development.connection;
       } else {
         connection = knexConfig.staging.connection;
@@ -24,7 +26,7 @@ module.exports = function() {
 
     knex = require('knex')({
       client: 'pg',
-      debug: true,
+      debug: debug,
       connection: connection
     });
     bookshelf = require('bookshelf')(knex);
